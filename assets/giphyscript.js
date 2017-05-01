3e99b7dc8467edb7360aa99c13a00f3ef1f295e0
paradigm1984 <danblanco1990@gmail.com>
2017-05-01 10:02:28 -0400

    began working on JS

--- assets/giphyscript.js
+++ assets/giphyscript.js
@@ -1,6 +1,38 @@
+$(document).ready(function) {
+
 /* ========================= VARIABLES ======================== */
 
 // API key in a var to reuse
 var authKey = "dc6zaTOxFJmzC";
 
-var animalCategory
+/* not sure if i needed to put the buttons i made in this array since
+im not clearing them but it cant hurt. new ones will be added here also.*/
+var animalCategories = ["Platypus", "Emu", "Honey Badger", "Turtle", "Koala"];
+
+function startingButtons() {
+	// clears div
+	$("#buttonContainer").empty(); 
+
+	// adds a button/class/attribute/name for each category in the array.
+    for (var i = 0; i < animalCategories.length; i++){
+        var gifButton = $("<button>");
+        gifButton.addClass("action");
+        gifButton.addClass("btn btn-primary")
+        gifButton.attr("data-name", animalCategories[i]);
+        gifButton.text(animalCategories[i]);
+        $("#buttonContainer").append(gifButton);
+    };
+};
+
+
+function addNewButton(){
+    
+};
+
+
+
+
+
+
+
+};
